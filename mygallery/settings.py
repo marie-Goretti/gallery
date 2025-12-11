5d6e56d7a1af5aae9e6c08d597b67240a974aa0a
Marie-Gorétti <gorettimgboouna@gmail.com>
2025-12-11 14:18:21 +0100

    correction erreur

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery',
-    'django_recaptcha'
+    'django_recaptcha',
     'cloudinary',
     'cloudinary_storage',
 ]
